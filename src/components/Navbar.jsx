225fde61c3118417a3d51b395485945f57f254f4
rahulatlamp <rahulrajan@lampros.tech>
2022-11-20 19:17:14 +0530

    navbar and homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "./navbar.scss";
 
 function Navbar() {
     return (
         <div className='nav-main'>
             <div className='nav-logo'>
-                <img className='nav-logo' height="50px" width="50px" src="/images/logo192.png" />
+                <img className='nav-logo-main' src="/images/AllThatData.png" alt="logo" />
             </div>
-            <div className='nav-controls'>
-                <ul className='nav-items'>
-                    <li className='nav-item'><Link to="/">Home</Link></li>
-                    <li className='nav-item'><Link to="/organisation">Organisation</Link></li>
-                    <li className='nav-item'><Link to="/files">Files</Link></li>
-                    <li className='nav-item'><Link to="/profile">Profile</Link></li>
-                    <li className='nav-item'><button>Connect</button></li>
-                </ul>
-            </div>
+            <ul className='nav-items'>
+                <li className='nav-item'><Link to="/">Home</Link></li>
+                <li className='nav-item'><Link to="/organisation">Organisation</Link></li>
+                <li className='nav-item'><Link to="/files">Files</Link></li>
+                <li className='nav-item'><Link to="/profile">Profile</Link></li>
+                <li className='nav-item'><button className='connect-btn'>Connect</button></li>
+            </ul>
         </div>
     )
 }
