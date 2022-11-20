b1fdd613db1043af815405d8591f7644552a177f
Adithya Srinivasan <s.adithya.srinivas@gmail.com>
2022-11-20 20:46:03 +0530

    Files and Organization

--- src/components/organization/Organization.jsx
+++ src/components/organization/Organization.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./organization.scss";
 
 function Organization() {
   return (
-    <div>Organization</div>
-  )
+    <div>
+      <div className="org-sub">
+        <img
+          className="org-img"
+          src="https://logos-world.net/wp-content/uploads/2021/03/World-Health-Organization-WHO-Emblem.png"
+        />
+        <div className="org-header">Organization Name</div>
+        <div className="org-txt">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum velit
+          officia illo facere minus veniam quae quia molestias ipsa eum neque
+          cupiditate in officiis, quod tenetur architecto quidem error
+          voluptate.
+        </div>
+        <div className="org-tag">Tagline</div>
+      </div>
+    </div>
+  );
 }
 
-export default Organization
+export default Organization;
